879b7fdadb5a9dc93de0e8d7be9b52ca9ea469b0
saulesc <saulescfn@gmail.com>
2022-03-28 22:22:38 +0200

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8801',
+  smartContractInvokeApi: '/invoke/my-channel1/chaincode1',
+  smartContractQueryApi: '/query/my-channel1/chaincode1'
 };
